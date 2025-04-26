54f5eb678ab4e7d62d6830658d6ac3ce96321f86
Shaik Abdul Kader <shaikabdulkader.s00@gmail.com>
2025-04-26 16:13:33 +0530

    16 to 20 Updated

--- 18-01445-apples-&-oranges.sql
+++ 18-01445-apples-&-oranges.sql
@@ -1,20 +1,74 @@
+-- 1445. Apples & Oranges
+
+/*
+
+Table: Sales
+    +---------------+---------+
+    | Column Name   | Type    |
+    +---------------+---------+
+    | sale_date     | date    |
+    | fruit         | enum    |
+    | sold_num      | int     |
+    +---------------+---------+
+(sale_date,fruit) is the primary key for this table.
+This table contains the sales of "apples" and "oranges" sold each day.
+
+
+Write an SQL query to report the difference between number of apples and oranges sold each day.
+    Return the result table ordered by sale_date in format ('YYYY-MM-DD').
+
+
+The query result format is in the following example:
+
+Sales table:
+    +------------+------------+-------------+
+    | sale_date  | fruit      | sold_num    |
+    +------------+------------+-------------+
+    | 2020-05-01 | apples     | 10          |
+    | 2020-05-01 | oranges    | 8           |
+    | 2020-05-02 | apples     | 15          |
+    | 2020-05-02 | oranges    | 15          |
+    | 2020-05-03 | apples     | 20          |
+    | 2020-05-03 | oranges    | 0           |
+    | 2020-05-04 | apples     | 15          |
+    | 2020-05-04 | oranges    | 16          |
+    +------------+------------+-------------+
+
+Result table:
+    +------------+--------------+
+    | sale_date  | diff         |
+    +------------+--------------+
+    | 2020-05-01 | 2            |
+    | 2020-05-02 | 0            |
+    | 2020-05-03 | 20           |
+    | 2020-05-04 | -1           |
+    +------------+--------------+
+
+Expl:
+    Day 2020-05-01, 10 apples and 8 oranges were sold (Difference  10 - 8 = 2).
+    Day 2020-05-02, 15 apples and 15 oranges were sold (Difference 15 - 15 = 0).
+    Day 2020-05-03, 20 apples and 0 oranges were sold (Difference 20 - 0 = 20).
+    Day 2020-05-04, 15 apples and 16 oranges were sold (Difference 15 - 16 = -1).
+
+*/
+
 -- simple aggregate with condition
 
-select sale_date, (sum(case when fruit = 'apples' then sold_num else 0 end) - 
-                  sum(case when fruit = 'oranges' then sold_num else 0 end)) as diff
-from Sales
-group by 1
-order by 1
+SELECT sale_date, (SUM(case when fruit = 'apples' then sold_num else 0 end) - 
+                  SUM(case when fruit = 'oranges' then sold_num else 0 end)) as diff
+FROM Sales
+GROUP BY 1
+ORDER BY 1;
 
----------------------------------------------------------------------------------------------------------------  
+
 -- using join- 1 table for apples, 1 for oranges, join on sales date
 
-select sa.sale_date, (ifnull(sum(sa.sold_num),0)-ifnull(sum(so.sold_num), 0)) as diff
-from Sales sa
-join Sales so
-on sa.sale_date = so.sale_date and sa.fruit = 'apples' and so.fruit = 'oranges'
-group by 1
-order by 1
+SELECT a.sale_date, (COALESCE(SUM(a.sold_num), 0) - COALESCE(SUM(o.sold_num), 0)) as diff
+FROM Sales a
+JOIN Sales o
+    ON a.sale_date = o.sale_date AND a.fruit = 'apples' and o.fruit = 'oranges'
+GROUP BY 1
+ORDER BY 1;
 
 
--- facebook- 1
+
--- 19-01699-number-of-calls-between-two-persons.sql
+++ 19-01699-number-of-calls-between-two-persons.sql
@@ -1,27 +1,60 @@
--- union all- this gets all calls, then we put condition p1 < p2
+-- 1699. Number of Calls Between Two Persons
 
-select from_id as person1, to_id as person2, count(*) as call_count, sum(duration) as total_duration 
-from
-    (select from_id, to_id, duration
-    from Calls
-    union all
-    select to_id, from_id, duration
-    from Calls) t
-where from_id < to_id
-group by 1, 2
+/*
 
------------------------------------------------------------------------------------------------------------------------------
--- without using union all
--- make p1 < p2, then do calculations
-  
-select 
-(case when from_id < to_id then from_id else to_id end) as person1,
-(case when from_id < to_id then to_id else from_id end) as person2,
-count(*) as call_count,
-sum(duration) as total_duration
-from Calls
-group by 1, 2
+Table: Calls
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | from_id     | int     |
+    | to_id       | int     |
+    | duration    | int     |
+    +-------------+---------+
+
+    This table does not have a primary key, it may contain duplicates.
+    This table contains the duration of a phone call between from_id and to_id.
+    from_id != to_id
 
 
--- facebook- 2
--- amazon- 1
+Write an SQL query to report the number of calls and the total call duration between each pair of distinct persons
+    (person1, person2) where person1 < person2.
+Return the result table in any order.
+The query result format is in the following example:
+
+
+
+Calls table:
+    +---------+-------+----------+
+    | from_id | to_id | duration |
+    +---------+-------+----------+
+    | 1       | 2     | 59       |
+    | 2       | 1     | 11       |
+    | 1       | 3     | 20       |
+    | 3       | 4     | 100      |
+    | 3       | 4     | 200      |
+    | 3       | 4     | 200      |
+    | 4       | 3     | 499      |
+    +---------+-------+----------+
+
+Result table:
+    +---------+---------+------------+----------------+
+    | person1 | person2 | call_count | total_duration |
+    +---------+---------+------------+----------------+
+    | 1       | 2       | 2          | 70             |
+    | 1       | 3       | 1          | 20             |
+    | 3       | 4       | 4          | 999            |
+    +---------+---------+------------+----------------+
+
+Expl:
+    Users 1 and 2 had 2 calls and the total duration is 70 (59 + 11).
+    Users 1 and 3 had 1 call and the total duration is 20.
+    Users 3 and 4 had 4 calls and the total duration is 999 (100 + 200 + 200 + 499).
+
+*/
+
+
+SELECT (CASE WHEN from_id < to_id THEN from_id ELSE to_id END) AS p1,
+    (CASE WHEN from_id < to_id THEN to_id ELSE from_id END) AS p2,
+    COUNT(*) AS call_count, SUM(duration) AS total_duration
+FROM Calls
+GROUP BY 1, 2;
